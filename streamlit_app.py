4af97834a21a71b5b14065b2aa645a2332c2ad3b
lolawang22 <99853120+lolawang22@users.noreply.github.com>
2024-03-02 00:39:44 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+### Main Page ###
+
+### Page 1 ###
+## Question: What is the relationship between the patients’ demographic and the outcomes of arterial catheterization?
+
+### Page 2 ###
+## Question: How do initial clinical assessments and vital signs correlate with the length of stay in the ICU and hospital?
+
+### Page 3 ###
+## Question: Does the time of day or week of ICU admission affect patients’ outcomes or complications?
